f8a8f95d6d4cbc20e58ca13493b4c4f07e4de22b
sapta94 <dey7.kol@gmail.com>
2020-04-16 22:47:26 +0530

    added endpoints for create auth token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -6,6 +6,7 @@ from rest_framework.test import APIClient
 from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
+TOKEN_URL = reverse('user:token')
 
 def create_user(**params):
     return get_user_model().objects.create_user(**params)
